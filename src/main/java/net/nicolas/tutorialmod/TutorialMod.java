78ae1ad080203130f5cc2db26dee777f3aeede56
Nicolas <luebstorf.nicolas5@gmail.com>
2024-09-20 21:50:23 -0500

    Creative Tabs

--- src/main/java/net/nicolas/tutorialmod/TutorialMod.java
+++ src/main/java/net/nicolas/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.nicolas.tutorialmod;
 
 import net.minecraft.world.item.CreativeModeTabs;
 import net.nicolas.tutorialmod.block.ModBlocks;
+import net.nicolas.tutorialmod.item.ModCreativeModeTabs;
 import net.nicolas.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -38,6 +39,8 @@ public class TutorialMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
